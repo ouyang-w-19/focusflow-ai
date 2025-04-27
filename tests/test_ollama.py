eaac7b3e4cda18b8d9ded0f87c72677744ceb9f0
ouyang-w-19 <20649649+ouyang-w-19@users.noreply.github.com>
2025-04-27 23:39:43 +0200

    Remove unused modules in folder agents and tests

--- tests/test_ollama.py
+++ tests/test_ollama.py
@@ -1,7 +1,13 @@
 from config import OLLAMA_HOST
 from langchain_ollama import OllamaLLM
-
+import time
 # print("OLLAMA_HOST =", OLLAMA_HOST)
-llm = OllamaLLM(model="qwen2:7b")
+model_list = ["qwen2:7b", "qwen2.5:3b", "deepseek-r1:1.5b"]
+model_name = model_list[2]
+print(model_name)
+llm = OllamaLLM(model=model_name)
+start_time = time.time()
 print(llm.invoke("Give me 3 time management tips."))
-
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"\nTime taken: {elapsed_time:.2f} seconds.")
